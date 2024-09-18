762fc01a4d144efec092cc30efaa8fe3351fa815
Gustavo Figueiredo <gustavoribeiro665@hotmail.com>
2024-09-18 08:57:00 -0300

    feat: 1- two sum

--- two_sum/two-sum-3.go
+++ two_sum/two-sum-3.go
@@ -1,11 +1,42 @@
 package two_sum
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 func init() {
-	fmt.Println(twoSum([]int {3, 3}, 6))
+	fmt.Println(twoSum([]int {1, 10, 11, 12, 13, 2}, 3))
 }
 
 func twoSum(nums []int, target int) []int {
+	hashmap := make(map[int]int)
+
+	for idx, val := range nums {
+		complemento := target - val
+		
+		if mapHasKey(hashmap, val) {
+			return []int{hashmap[val], idx}
+		}
+		hashmap[complemento] = idx
+	}
 	return []int {}
+}
+
+func mapHasKey(hashMap map[int]int, key int) bool {
+	keys := make([]int, 0, len(hashMap))
+	for k := range hashMap {
+		keys = append(keys, k)
+	}
+	_, error := indexOf(keys, key)
+	return error == nil
+}
+
+func indexOf(keys []int, element int) (int, error) {
+	for k, v := range keys {
+		if element == v {
+			return k, nil
+		}
+	}
+	return 0, errors.New("chave não encontrada")
 }
